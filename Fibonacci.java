9ddd51e20b5dc88917ef2f4ef7bcb6f273fe2f9e
Tatiana Fucsik <108027021+Tatianaf7@users.noreply.github.com>
2025-01-15 11:09:24 -0500

    Add Javadoc comments to Fibonacci method

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,12 +1,18 @@
 package cen4802;
 
 public class Fibonacci {
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+	/**
+	 * Calculates the nth term in the Fibonacci sequence.
+	 *
+	 * @param n the position of the term in the Fibonacci sequence
+	 * @return the nth Fibonacci number
+	 */
+	public static int fibonacci(int n) {
+	    if (n <= 1) {
+	        return n;
+	    }
+	    return fibonacci(n - 1) + fibonacci(n - 2);
+	}
 
     public static void main(String[] args) {
         int n = 10;
